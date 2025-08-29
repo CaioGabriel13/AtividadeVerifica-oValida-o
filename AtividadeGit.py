334a37ae4eff5a27c5150921702d2cfe78611a9c
julianaflorss_ <julianaforesflores012@gmail.com>
2025-08-29 20:34:10 -0300

    Código Revisado está ok

--- AtividadeGit.py
+++ AtividadeGit.py
@@ -1,15 +1,21 @@
 def is_prime(n):
+
     if n <= 1:
         return False
+    
     if n == 2:
         return True
+    
     if n % 2 == 0:
         return False
+    
     for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
+
 if __name__ == "__main__":
+   
     try:
         numero = int(input("Digite número inteiro para verificar se é primo:"))
         resultado = is_prime(numero)
