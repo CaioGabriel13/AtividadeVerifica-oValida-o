b241478a2fb2a22b99011e749d2a606a7b3dccd4
CaioGabriel13 <caiogabrielsilvas@gmail.com>
2025-08-28 21:12:57 -0300

    Atividade Finalizada!

--- AtividadeGit.py
+++ AtividadeGit.py
@@ -11,13 +11,13 @@ def is_prime(n):
     return True
 if __name__ == "__main__":
     try:
-        numero = int(input("Digite um número inteiro para verificar se é primo:"))
+        numero = int(input("Digite número inteiro para verificar se é primo:"))
         resultado = is_prime(numero)
         
         if resultado:
-            print(f"O número {numero} É PRIMO!")
+            print(f"Número {numero} É PRIMO")
         else:
-            print(f"O número {numero} NÃO É PRIMO.")
+            print(f"Número {numero} NÃO É PRIMO.")
             
     except ValueError:
         print("Por favor, digite apenas números inteiros válidos.")
